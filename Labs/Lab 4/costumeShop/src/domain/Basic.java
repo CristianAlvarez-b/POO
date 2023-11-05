cdcc19a0180fe5b1a7431a38f55144a276360a9f
Shiro <cristian.alvarez-b@mail.escuelaing.edu.co>
2023-11-04 20:00:28 -0500

    "ahi vamos"

--- Labs/Lab 4/costumeShop/src/domain/Basic.java
+++ Labs/Lab 4/costumeShop/src/domain/Basic.java
@@ -3,14 +3,26 @@ package domain;
 public class Basic extends Costume{
     
     private Integer price;
-    
+
+    /**
+     * Constructs a new basic costume.
+     *
+     * @param name     The name of the basic costume.
+     * @param price    The price of the basic costume.
+     * @param discount The discount percentage for the costume.
+     */
     public Basic(String name, Integer price, int discount){
         this.name=name;
         this.price=price;
         this.discount=discount;
-    }    
-    
+    }
 
+    /**
+     * Calculates the price of the basic costume, considering the price and discount.
+     *
+     * @return The total price of the basic costume.
+     * @throws CostumeShopException If there is an error in the price calculation.
+     */
     @Override
     public int price() throws CostumeShopException{
        if (price == null) throw new CostumeShopException(CostumeShopException.PRICE_EMPTY);
