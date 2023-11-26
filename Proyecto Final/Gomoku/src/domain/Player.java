bb83f1b6679a50ce60bbd7aaf0e76710069a5bb3
bricenojuliana <bricenojuliana@gmail.com>
2023-11-25 21:39:21 -0500

    ahi vamos

--- Proyecto Final/Gomoku/src/domain/Player.java
+++ Proyecto Final/Gomoku/src/domain/Player.java
@@ -1,11 +1,13 @@
 package domain;
-import java.util.ArrayList;
+import java.util.*;
 import java.awt.*;
 
+
 public abstract class Player {
-    protected int remainingStones;
+    protected ArrayList<Stone> remainingStones;
+    protected boolean canRefill;
     protected Color color;
-    ArrayList<Stone> extraStones;
+    protected ArrayList<Stone> extraStones;
     protected Board board;
     protected int punctuation;
 
@@ -17,6 +19,37 @@ public abstract class Player {
 
     public void eliminateExtraStone(int index){
         extraStones.remove(index);
+    }
+
+    public void refillStones(int size, int specialStonesPercentage) {
+        if (canRefill) {
+            remainingStones.clear(); // Limpiar las piedras restantes
+
+            // Calcular la cantidad de piedras especiales según el porcentaje
+            int totalSpecialStones = (int) (size * specialStonesPercentage / 100.0);
+
+            // Distribuir aleatoriamente las piedras especiales entre Temporary y Heavy
+            int totalTemporaryStones = new Random().nextInt(totalSpecialStones + 1); // Aleatorio entre 0 y totalSpecialStones
+            int totalHeavyStones = totalSpecialStones - totalTemporaryStones;
+
+            // Llenar el ArrayList con piedras especiales
+            for (int i = 0; i < totalTemporaryStones; i++) {
+                remainingStones.add(new Temporary(color));
+            }
+
+            for (int i = 0; i < totalHeavyStones; i++) {
+                remainingStones.add(new Heavy(color));
+            }
+
+            // Calcular la cantidad de piedras normales
+            int totalNormalStones = size - totalSpecialStones;
+
+            // Llenar el ArrayList con piedras normales
+            for (int i = 0; i < totalNormalStones; i++) {
+                remainingStones.add(new Stone(color));
+            }
+
+        }
     }
 
     public Color getColor() {return color;}
