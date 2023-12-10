aa4961436b0d7cd648b32f403dfa87fa74e493af
bricenojuliana <bricenojuliana@gmail.com>
2023-12-09 20:48:29 -0500

    domain: arreglando limitedStones

--- Proyecto Final/Gomoku/src/domain/LimitedStones.java
+++ Proyecto Final/Gomoku/src/domain/LimitedStones.java
@@ -4,6 +4,11 @@ public class LimitedStones extends Board{
 
     public LimitedStones(int rows, int columns) throws Exception {
         super(rows, columns, 0);
+    }
+
+    @Override
+    public void setPlayers(Player[] players) {
+        this.players = players;
         players[0].setCanRefill(false);
         players[1].setCanRefill(false);
     }
